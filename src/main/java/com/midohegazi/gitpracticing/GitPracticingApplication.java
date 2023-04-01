0339ea2c295d2a945208d8d25a17576f51910032
Mohamed <m.hegazi@thecrypto.farm>
2023-04-01 19:40:36 +0200

    second commit - after initialization deleting un necessary files

---
+++ src/main/java/com/midohegazi/gitpracticing/GitPracticingApplication.java
@@ -0,0 +1,13 @@
+package com.midohegazi.gitpracticing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticingApplication.class, args);
+	}
+
+}
